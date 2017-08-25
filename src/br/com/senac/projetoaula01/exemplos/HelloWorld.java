7e82313cb7b504bf439eeeda2d7916b108fc506a
51115152016.5 <51115152016.5@51DE194277.senacrjedu.local>
2017-08-25 16:56:50 -0300

    Primeiro commit

---
+++ src/br/com/senac/projetoaula01/exemplos/HelloWorld.java
@@ -0,0 +1,10 @@
+package br.com.senac.projetoaula01.exemplos;
+
+// cabeçalho de declaração da classe
+public class HelloWorld {
+
+	// método que executa sua aplicação
+	public static void main(String[] args) {
+		System.out.println("Hello Java World!");
+	}
+}
