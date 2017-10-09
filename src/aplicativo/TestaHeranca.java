7b9945dc7d4de3149fc559972937ee6b55d71d9e
51115152016.5 <51115152016.5@51DE194277.senacrjedu.local>
2017-10-09 16:18:10 -0300

    CRUD

--- src/aplicativo/TestaHeranca.java
+++ src/aplicativo/TestaHeranca.java
@@ -1,7 +1,10 @@
 package aplicativo;
 
+import java.sql.SQLException;
+
 import entidade.Cliente;
 import entidade.Funcionario;
+import persistencia.FuncionarioDAO;
 
 public class TestaHeranca {
 	public static void main(String[] args) {
@@ -19,11 +22,22 @@ public class TestaHeranca {
 		
 		funcionario.setId(2L);
 		funcionario.setNome("Maria");
+		funcionario.setCpf("987.654.321-99");
 		funcionario.setMatricula(1234L);
 		funcionario.setEmail("maria@gmail.com");
 		funcionario.setSalario(10000.0);
 		funcionario.setLogin("maria");
 		funcionario.setSenha("123");
 		funcionario.setSexo("Feminino");
+		
+		FuncionarioDAO fd = new FuncionarioDAO();
+		
+		try {
+			fd.salvar(funcionario);
+			System.out.println("Funcionario Salvo com sucesso");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Erro ao gravar no banco");
+		}
 	}
 }
--- src/persistencia/FuncionarioDAO.java
+++ src/persistencia/FuncionarioDAO.java
@@ -1,5 +1,53 @@
 package persistencia;
 
-public class FuncionarioDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import entidade.Funcionario;
+
+public class FuncionarioDAO extends ConnectionDAO {
+
+	private Connection conn;
+	private PreparedStatement stmt;
+
+	public FuncionarioDAO() {
+		try {
+			conn = getConnection();
+			stmt = null;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void salvar(Funcionario f) throws SQLException {
+
+		try {
+
+			stmt = conn.prepareStatement("insert into funcionario values (null, ?, ?, ?, ?, ?, md5(?), ?, ?)");
+
+			stmt.setString(1, f.getNome());
+			stmt.setString(2, f.getCpf());
+			stmt.setString(3, f.getSexo());
+			stmt.setString(4, f.getEmail());
+			stmt.setString(5, f.getLogin());
+			stmt.setString(6, f.getSenha());
+			stmt.setDouble(7, f.getSalario());
+			stmt.setLong(8, f.getMatricula());
+
+			boolean flag = stmt.execute();
+
+			if (flag) {
+				throw new SQLException("Erro ao inserir o funcionario");
+			}
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+
+			if (conn != null) {
+				conn.close();
+			}
+		}
+	}
 }
